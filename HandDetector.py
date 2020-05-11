46aec1f1631335008d62fd1e9971b6ecdea88767
skdw <kamil.gorzynski8@gmail.com>
2020-05-11 12:44:48 +0200

    Keypoints filtering added to HandDetector

--- HandDetector.py
+++ HandDetector.py
@@ -91,17 +91,22 @@ class HandDetector():
         hsv_image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
         H,S,V = cv2.split(hsv_image)
 
-        hh = sorted(H[int(y)][int(x)] for x, y in keypoints)
-        ss = sorted(S[int(y)][int(x)] for x, y in keypoints)
-        vv = sorted(V[int(y)][int(x)] for x, y in keypoints)
+        keypoints_ok = [point for point in keypoints 
+                        if point[0] >= 0 and point[0] < image.shape[1] 
+                        and point[1] >= 0 and point[1] < image.shape[0]]
+
+        hh = sorted(H[int(y)][int(x)] for x, y in keypoints_ok)
+        ss = sorted(S[int(y)][int(x)] for x, y in keypoints_ok)
+        vv = sorted(V[int(y)][int(x)] for x, y in keypoints_ok)
 
         lower = np.array([hh[0], ss[0], 0])
         upper = np.array([hh[-1], ss[-1], 255])
 
         mask = cv2.inRange(hsv_image, lower, upper)
         return mask
-    
+
 if __name__ == '__main__':
+    curr_dir = os.getcwd()
     palm_model_path = curr_dir + "/models/palm_detection.tflite"
     anchors_path = curr_dir + "/models/anchors.csv"
 
